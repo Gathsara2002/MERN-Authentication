bf9458bddc9664d14d3a23e0e988c1b149bfa2e8
Gathsara2002 <liviru2002mmg@gmail.com>
2024-03-07 20:02:41 +0530

    user route updated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,9 @@
 const loginUser = async (req, res) => {
   res.json({ msg: "login user" });
 };
+
+const singupUser = async (req, res) => {
+  res.json({ msg: "signup user" });
+};
+
+module.exports = { loginUser, singupUser };
