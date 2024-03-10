d2808bbe5356c132bdd5a95250bdf041af1c50a0
Gathsara2002 <liviru2002mmg@gmail.com>
2024-03-10 21:02:46 +0530

    create and send token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,6 @@
 const userModel = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const loginUser = async (req, res) => {
   res.json({ msg: "login user" });
@@ -9,10 +11,19 @@ const singupUser = async (req, res) => {
 
   try {
     const user = await userModel.signupMethod(email, password);
-    res.status(200).json({ user:user });
+
+    //create token and send in response
+    const token = createToken(user._id);
+
+    res.status(200).json({ user: user, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
+};
+
+//create token
+const createToken = (_id) => {
+  return jwt.sign({ _id: _id }, process.env.SECRET, { expiresIn: "5d" });
 };
 
 module.exports = { loginUser, singupUser };
