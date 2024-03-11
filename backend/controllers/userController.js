fafddc93fec923f4dfb484f923a9e31dee934f34
Gathsara2002 <liviru2002mmg@gmail.com>
2024-03-11 19:50:28 +0530

    login user controller updated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,18 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const loginUser = async (req, res) => {
-  res.json({ msg: "login user" });
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+
+    // create a token
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 const singupUser = async (req, res) => {
